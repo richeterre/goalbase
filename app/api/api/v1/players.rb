81781ec7318d96b823839357093a4ed3c7e28542
Martin Richter <me@martinrichter.net>
2015-04-12 18:30:24 +0200

    Allow creation of new players with a mandatory name

--- app/api/api/v1/players.rb
+++ app/api/api/v1/players.rb
@@ -2,9 +2,19 @@ module API
   module V1
     class Players < Grape::API
       resource :players do
-        desc "Return list of all players"
+        desc "Return a list of all players"
         get do
           Player.all
+        end
+
+        desc "Create a new player"
+        params do
+          requires :name, type: String, desc: "The player's name"
+        end
+        post do
+          Player.create!({
+            name: params[:name]
+          })
         end
       end
     end
