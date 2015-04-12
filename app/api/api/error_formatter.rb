650482c15f1958351b21bbc1a1fb5d6bd311332c
Martin Richter <me@martinrichter.net>
2015-04-12 18:19:24 +0200

    Add error formatter with JSON output

---
+++ app/api/api/error_formatter.rb
@@ -0,0 +1,7 @@
+module API
+  module ErrorFormatter
+    def self.call message, backtrace, options, env
+      { response_type: 'error', response: message }.to_json
+    end
+  end
+end
--- app/api/api/root.rb
+++ app/api/api/root.rb
@@ -2,6 +2,8 @@ module API
   class Root < Grape::API
     prefix :api
     format :json
+    error_formatter :json, API::ErrorFormatter
+
     mount API::V1::Root
   end
 end
