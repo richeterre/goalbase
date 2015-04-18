ce6285570c7c6384559dfc62fa5311f5b2d168ee
Martin Richter <martin.richter@futurice.com>
2015-04-18 20:30:23 +0200

    Fix incorrect documentation syntax for match parameters

--- app/api/api/v1/matches.rb
+++ app/api/api/v1/matches.rb
@@ -19,10 +19,10 @@ module API
 
         desc "Create a new match"
         params do
-          requires :home_goals, type: Integer, "Goals scored by home players"
-          requires :away_goals, type: Integer, "Goals scored by away players"
-          requires :home_player_uuids, type: Array[String], "The match's home players"
-          requires :away_player_uuids, type: Array[String], "The match's away players"
+          requires :home_goals, type: Integer, desc: "Goals scored by home players"
+          requires :away_goals, type: Integer, desc: "Goals scored by away players"
+          requires :home_player_uuids, type: Array[String], desc: "The match's home players"
+          requires :away_player_uuids, type: Array[String], desc: "The match's away players"
         end
         post do
           home_players = Player.where(uuid: params[:home_player_uuids])
