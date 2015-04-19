c84ab14bf22f8e98deffbd004d4d5f4f895fa620
Martin Richter <martin.richter@futurice.com>
2015-04-19 16:43:13 +0200

    Allow editing of matches via API

--- app/api/api/v1/matches.rb
+++ app/api/api/v1/matches.rb
@@ -35,6 +35,33 @@ module API
             away_players: away_players
           })
         end
+
+        route_param :id do
+          desc "Updates a match"
+          params do
+            requires :home_goals, type: Integer, desc: "Goals scored by home players"
+            requires :away_goals, type: Integer, desc: "Goals scored by away players"
+            requires :home_player_ids, type: Array[String], desc: "IDs of home players in the match"
+            requires :away_player_ids, type: Array[String], desc: "IDs of away players in the match"
+          end
+          put do
+            home_players = Player.find(params[:home_player_ids])
+            away_players = Player.find(params[:away_player_ids])
+            match = Match.find(params[:id])
+            match.update!({
+              home_goals: params[:home_goals],
+              away_goals: params[:away_goals],
+              home_players: home_players,
+              away_players: away_players
+            })
+            return match
+          end
+
+          desc "Deletes a match"
+          delete do
+            Match.find(params[:id]).destroy
+          end
+        end
       end
     end
   end
