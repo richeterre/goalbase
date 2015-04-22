b16d224d0ec8844c636fd8fa845cb2be4ce8e4a5
Martin Richter <martin.richter@futurice.com>
2015-04-22 19:31:16 +0200

    Test match listing and creation

---
+++ spec/api/matches_spec.rb
@@ -0,0 +1,64 @@
+require 'rails_helper'
+
+describe API::V1 do
+  matches_path = "/api/v1/matches"
+
+  describe "GET #{matches_path}" do
+    it "returns an empty array of matches" do
+      get matches_path
+      expect(response.status).to eq(200)
+      expect(JSON.parse(response.body)).to eq []
+    end
+  end
+
+  context "when a match exists" do
+    describe "GET #{matches_path}" do
+      it "returns the match" do
+        @match = FactoryGirl.create(:match)
+        @home_player = @match.home_players[0]
+        @away_player = @match.away_players[0]
+
+        get matches_path
+        expect(response.status).to eq(200)
+        expect(response.body).to eq [
+          {
+            id: @match.id,
+            home_goals: @match.home_goals,
+            away_goals: @match.away_goals,
+            home_players: [
+              id: @home_player.id,
+              name: @home_player.name
+            ],
+            away_players: [
+              id: @away_player.id,
+              name: @away_player.name
+            ]
+          }
+        ].to_json
+      end
+    end
+  end
+
+  context "with valid attributes" do
+    before (:each) do
+      match = FactoryGirl.create(:match)
+      @match_params = {
+          home_goals: match.home_goals,
+          away_goals: match.away_goals,
+          home_player_ids: match.home_players.map(&:id),
+          away_player_ids: match.away_players.map(&:id)
+        }
+    end
+
+    it "creates a new match" do
+      expect {
+        post matches_path, @match_params
+      }.to change(Match, :count).by(1)
+    end
+
+    it "has the right response status code when creating a new match" do
+      post matches_path, @match_params
+      expect(response.status).to eq(201)
+    end
+  end
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,11 @@ FactoryGirl.define do
   factory :player do
     name 'foo'
   end
+
+  factory :match do
+    home_goals 2
+    away_goals 1
+    home_players {[FactoryGirl.create(:player)]}
+    away_players {[FactoryGirl.create(:player)]}
+  end
 end
